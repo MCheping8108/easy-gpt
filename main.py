4dcc0e9e02069050b93823a42751ac2243c0504a
MCheping8108 <liuyiming53@outlook.com>
2023-11-22 18:27:26 +0800

    支持输入更多模型

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ client = OpenAI(
     api_key="sk-xxx",
 )
 
+list_model = ["gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106", "gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-32k", "gpt-4-32k-0314", "gpt-4-1106-preview", "gpt-4-vision-preview"]
+
 content = easygui.enterbox("请输入内容")
 
-choose_model = easygui.choicebox("请选择模型", choices=["gpt-3.5-turbo", "gpt-4"])
+choose_model = easygui.choicebox("请选择模型", choices=list_model)
 
 chat_completion = client.chat.completions.create(
     model=choose_model,
